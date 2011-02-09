32981a1fc7616cb00bc623a1df7b4f265bb79094
Nicholas Wang <clone.of.snake@gmail.com>
2011-02-09 22:33:50 +0800

    up till ch.7 before Refactoring

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '093adbb78aec448e11370090ce514dce8aa94c67159072c8885baf8cd480ddbd503d26b01f93416fa3298c05948e0a3f99dd255fcb9154a5c577f62c8f237ae6'
